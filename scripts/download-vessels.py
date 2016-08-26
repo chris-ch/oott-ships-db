0f37e27603e2ce65c7235ed0e09fc258b6695917
Christophe Alexandre <chris.perso@gmail.com>
2016-08-26 14:48:20 +0200

    dev

---
+++ scripts/download-vessels.py
@@ -0,0 +1,92 @@
+import argparse
+import csv
+import logging
+from string import Template
+
+from bs4 import BeautifulSoup
+
+from urlcaching import set_cache_http, open_url
+
+_VESSEL_TYPES = {
+    'Cargo ships': '4',
+    'Tanker': '6'
+}
+
+_URL_BASE = 'https://www.vesselfinder.com'
+_URL_LIST_TEMPLATE = Template(_URL_BASE + '/vessels?t=$vessel_type&page=$page_count')
+
+
+def load_page(page_current, page_max=None):
+    assert page_current > 0
+    vessel_type = 'Tanker'
+    url = _URL_LIST_TEMPLATE.substitute({'vessel_type': _VESSEL_TYPES[vessel_type], 'page_count': page_current})
+    html_text = open_url(url)
+    html = BeautifulSoup(html_text, 'html.parser')
+    ship_rows = html.find_all('div', {'class': 'ship-row-details'})
+    page_content = list()
+    for ship_row in ship_rows:
+        ship_header = ship_row.find_next('header')
+        ship_details_url_path = ship_header.find_next('a', {'rel': 'bookmark'})['href']
+        mmsi_lookup = ship_details_url_path.split('-MMSI-')
+        ship_MMSI = ''
+        if len(mmsi_lookup) > 0:
+            ship_MMSI = mmsi_lookup[-1]
+
+        ship_country_owner = ship_header.find_next('a', {'rel': 'bookmark'}).find_next('img', {'class': 'ship-flag'})['title'][len('Flag of') + 1:]
+        ship_name = ship_header.find_next('a', {'rel': 'bookmark'}).text.strip()
+        row_data = {'ship_name': ship_name, 'ship_country_owner': ship_country_owner,
+                    'ship_details_url_path': ship_details_url_path, 'ship_MMSI': ship_MMSI}
+
+        for row_param in ship_row.find_all('div', {'class': 'row param'}):
+            param_name = row_param.find_next('div')
+            param_value_raw = param_name.find_next('div')
+            if param_value_raw.text.strip().upper() == 'N/A':
+                param_value = ''
+
+            else:
+                param_value = param_value_raw.text
+
+            row_data[param_name.text] = param_value
+
+        page_content.append(row_data)
+
+    pagination = html.find('div', {'id': 'vessels-list'}).find('div', {'class': 'row llc'}).find('ul', {'class': 'mypagination'})
+    page_last = pagination.find_next('li', {'class': 'last'}).find_next('a')['href'].split('&page=')[-1]
+    logging.info('processed page %s (last: %s, max: %s)', page_current, page_last, page_max)
+    completed = page_current >= int(page_last) or page_current >= (page_max, page_current + 1)[page_max is None]
+    return page_content, completed
+
+
+def load_pages(page_max=None):
+    page_counter = 1
+    results, completed = load_page(page_counter, page_max=page_max)
+    while not completed:
+        page_counter += 1
+        page_results, completed = load_page(page_counter, page_max=page_max)
+        results += page_results
+
+    with open('ship-db.csv', 'w') as ship_db:
+        csv_writer = csv.DictWriter(ship_db, results[0].keys())
+        csv_writer.writeheader()
+        csv_writer.writerows(results)
+
+
+def main():
+    load_pages(page_max=5)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+    logging.getLogger('requests').setLevel(logging.WARNING)
+    file_handler = logging.FileHandler('download-vessels.log', mode='w')
+    formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+    file_handler.setFormatter(formatter)
+    logging.getLogger().addHandler(file_handler)
+
+    parser = argparse.ArgumentParser(description='Importing vessels from online DB',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter
+                                     )
+
+    set_cache_http('output/urlcaching')
+
+    args = parser.parse_args()
+    main()
---
+++ src/urlcaching.py
@@ -0,0 +1,193 @@
+import logging
+import os
+
+import itertools
+import threading
+
+from datetime import datetime
+import requests
+import hashlib
+
+_CACHE_FILE_PATH = None
+_MAX_NODE_FILES = 0x400
+_REBALANCING_LIMIT = 0x1000
+_HEADERS_CHROME = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+_rebalancing = threading.Condition()
+
+
+def set_cache_http(cache_file_path):
+    global _CACHE_FILE_PATH
+    cache_file_path_full = os.path.abspath(cache_file_path)
+    _CACHE_FILE_PATH = cache_file_path_full
+    if not os.path.exists(_CACHE_FILE_PATH):
+        os.makedirs(_CACHE_FILE_PATH)
+
+    logging.debug('setting cache path: %s', cache_file_path_full)
+
+
+def is_cache_used():
+    return _CACHE_FILE_PATH is not None
+
+
+def _get_directories_under(path):
+    return (node for node in os.listdir(path) if os.path.isdir(os.path.join(path, node)))
+
+
+def _get_files_under(path):
+    return (node for node in os.listdir(path) if os.path.isfile(os.path.join(path, node)))
+
+
+def _generator_count(a_generator):
+    return sum(1 for item in a_generator)
+
+
+def _divide_node(path, nodes_path):
+    level = len(nodes_path)
+    new_node_sup_init = 'FF' * 20
+    new_node_inf_init = '7F' + 'FF' * 19
+    if level > 0:
+        new_node_sup = nodes_path[-1]
+        new_node_diff = (int(new_node_sup_init, 16) - int(new_node_inf_init, 16)) >> level
+        new_node_inf = '%0.40X' % (int(new_node_sup, 16) - new_node_diff)
+
+    else:
+        new_node_sup = new_node_sup_init
+        new_node_inf = new_node_inf_init
+
+    new_path_1 = os.path.sep.join([path] + nodes_path + [new_node_inf.lower()])
+    new_path_2 = os.path.sep.join([path] + nodes_path + [new_node_sup.lower()])
+    return os.path.abspath(new_path_1), os.path.abspath(new_path_2)
+
+
+def rebalance_cache_tree(path, nodes_path=None):
+    if not nodes_path:
+        nodes_path = list()
+
+    current_path = os.path.sep.join([path] + nodes_path)
+    files_node = _get_files_under(current_path)
+    rebalancing_required = _generator_count(itertools.islice(files_node, _MAX_NODE_FILES + 1)) > _MAX_NODE_FILES
+    if rebalancing_required:
+        new_path_1, new_path_2 = _divide_node(path, nodes_path)
+        logging.info('rebalancing required, creating nodes: %s and %s', os.path.abspath(new_path_1), os.path.abspath(new_path_2))
+        _rebalancing.acquire()
+        _rebalancing.wait()
+        if not os.path.exists(new_path_1):
+            os.makedirs(new_path_1)
+
+        if not os.path.exists(new_path_2):
+            os.makedirs(new_path_2)
+
+        for filename in _get_files_under(current_path):
+            file_path = os.path.sep.join([current_path, filename])
+            if file_path <= new_path_1:
+                logging.info('moving %s to %s', filename, new_path_1)
+                os.rename(file_path, os.path.sep.join([new_path_1, filename]))
+
+            else:
+                logging.info('moving %s to %s', filename, new_path_2)
+                os.rename(file_path, os.path.sep.join([new_path_2, filename]))
+
+        _rebalancing.release()
+
+    for directory in _get_directories_under(current_path):
+        rebalance_cache_tree(path, nodes_path + [directory])
+
+
+def find_node(digest, path=None):
+    if not path:
+        path = _CACHE_FILE_PATH
+
+    directories = sorted(_get_directories_under(path))
+
+    if not directories:
+        return path
+
+    else:
+        target_directory = None
+        for directory_name in directories:
+            if digest <= directory_name:
+                target_directory = directory_name
+                break
+
+        if not target_directory:
+            raise Exception('Inconsistent cache tree')
+
+        return find_node(digest, path=os.path.sep.join([path, target_directory]))
+
+
+def get_cache_filename(key):
+    hash_md5 = hashlib.md5()
+    hash_md5.update(key.encode('utf-8'))
+    digest = hash_md5.hexdigest()
+    target_node = find_node(digest)
+    cache_filename = os.sep.join([target_node, digest])
+    return cache_filename
+
+
+def is_cached(key):
+    cache_filename = get_cache_filename(key)
+    return os.path.exists(cache_filename)
+
+
+def file_size(filename):
+    count = -1
+    with open(filename) as file_lines:
+        for count, line in enumerate(file_lines):
+            pass
+
+    return count + 1
+
+
+def _add_to_cache(key, value):
+    _rebalancing.acquire()
+    try:
+        logging.debug('adding to cache: %s', key)
+        filename = get_cache_filename(key)
+        index_name = os.path.sep.join([_CACHE_FILE_PATH, 'index'])
+        today = datetime.today().strftime('%Y%m%d')
+        with open(filename, 'w') as cache_content:
+            cache_content.write(value)
+
+        with open(index_name, 'a') as index_file:
+            filename_digest = filename.split(os.path.sep)[-1]
+            index_file.write('%s %s: "%s"\n' % (today, filename_digest, key))
+
+    finally:
+        _rebalancing.notify_all()
+        _rebalancing.release()
+
+    if file_size(index_name) % _REBALANCING_LIMIT == 0:
+        logging.debug('rebalancing cache')
+        rebalance_cache_tree(_CACHE_FILE_PATH)
+
+
+def _get_from_cache(key):
+    _rebalancing.acquire()
+    try:
+        logging.debug('reading from cache: %s', key)
+        with open(get_cache_filename(key), 'r') as cache_content:
+            content = cache_content.read()
+
+    finally:
+        _rebalancing.notify_all()
+        _rebalancing.release()
+
+    return content
+
+
+def open_url(url):
+    logging.debug('opening url: %s', url)
+    if is_cache_used():
+        if not is_cached(url):
+            content = requests.get(url, headers=_HEADERS_CHROME).text
+            _add_to_cache(url, content)
+
+        content = _get_from_cache(url)
+
+    else:
+        # straight access
+        content = requests.get(url).text
+
+    return content
